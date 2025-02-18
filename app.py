b92f43f4379684746c4094d14da01d167293d037
Venkat Subramanian <vs9542@princeton.edu>
2025-02-18 00:03:18 -0500

    updating app

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def submit_response():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
